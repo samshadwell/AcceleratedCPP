828f68ac4400e83e50a91bf17025f1574593ace1
Samuel Shadwell <smshdwll@gmail.com>
2016-05-07 18:54:53 -0400

    Finished chapter 0 exercises

---
+++ 00/HelloHelloWorld.cpp
@@ -0,0 +1,16 @@
+// For exercise 0-4
+#include <iostream>
+
+int main()
+{
+    std::cout << "// a small C++ program" << std::endl;
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << std::endl;
+    std::cout << "int main()" << std::endl;
+    std::cout << "{" << std::endl;
+    std::cout << "\tstd::cout << \"Hello, world!\" << std::endl;" << std::endl;
+    std::cout << "\treturn 0;" << std::endl;
+    std::cout << "}" << std::endl;
+
+    return 0;
+}
---
+++ 00/HelloNewline.cpp
@@ -0,0 +1,10 @@
+// For exercise 0-10
+#include <iostream>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello,\nworld!" << std::endl;
+    return 0;
+}
